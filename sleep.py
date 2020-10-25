b45badd6899e45cc13265ceedc7c98c5fbfd6a5c
shreypjain <jainshreyp@gmail.com>
2020-10-25 13:37:38 -0400

    added sleep function, isn't done and shell script

--- sleep.py
+++ sleep.py
@@ -1,5 +1,17 @@
+from time import sleep
 from py_imessage import imessage
-import datetime
+import time
+
+phone ="7327998071"
+message ="Hello Sender! I am currently asleep, so if it is something urgent, I'm not sure if I could help you right at this moment, but if you need something for tomorrow then just leave a message I'll see it when I wake up"
 
 def sleepMessage():
+    hour = time.localtime(time.time()).tm_hour
     while True:
+        sleep(10)
+        if hour > 21 and hour < 5:
+            guid = imessage.send(phone, message)
+        else:
+            print(False)
+
+sleepMessage()
